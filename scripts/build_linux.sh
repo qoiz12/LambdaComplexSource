3beefd205a226cb6114574ed232b7a05e9205c18
Mohamed Ashraf <source2013inemscriptenlolol@gmail.com>
2025-04-26 17:46:49 +0300

    [fix][workflows] Fix certain YAML

---
+++ scripts/build_linux.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd src
+sudo dpkg --add-architecture i386
+sudo apt-get update
+sudo apt-get install -y aptitude
+sudo aptitude install -y libopenal-dev:i386 g++-multilib gcc-multilib libpng-dev:i386 libjpeg-dev:i386 libfreetype6-dev:i386 libfontconfig1-dev:i386 libcurl4-gnutls-dev:i386 libsdl2-dev:i386 zlib1g-dev:i386 libbz2-dev:i386 libedit-dev:i386
+cd src/ 
+./waf configure -T release && ./waf build
