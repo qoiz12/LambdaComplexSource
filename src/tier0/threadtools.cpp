9adce46e29da3113920a1f3525a02e5da73171c6
Mohamed Ashraf <source2013inemscriptenlolol@gmail.com>
2025-05-04 15:40:51 +0400

    [fix][tier0] Fixes for threadtools.cpp

--- src/tier0/threadtools.cpp
+++ src/tier0/threadtools.cpp
@@ -2748,7 +2748,9 @@ PLATFORM_INTERFACE void SetCurThreadPS3( CThread *pThread )
 #else
 // The CThread implementation needs to be inlined for performance on the PS3 - It makes a difference of more than 1ms/frame
 // for other platforms, we include the .inl in the .cpp file where it existed before
+#if !defined(__EMSCRIPTEN__)
 #include "../public/tier0/threadtools.inl"
+#endif
 #endif
 
 //-----------------------------------------------------------------------------
